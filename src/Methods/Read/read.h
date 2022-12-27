f1b6af7cb13e073222adf041fdf9c59331b24c9c
beta442 <grandoblivioniv@gmail.com>
2022-12-27 23:36:46 +0300

    'Lexeme' impl

--- src/Methods/Read/read.h
+++
@@ -1,3 +0,0 @@
-﻿#include <functional>
-#include "common.h"
-
--- src/interpreter/expression/IExpression.h
+++ src/interpreter/expression/IExpression.h
@@ -18,7 +18,7 @@ public:
 	// TODO: const? noexept? constexpr?
 	virtual IExpressionSharedPtr Copy() const = 0;
 	virtual void Evaluate(Context& context) = 0; // TODO: shared_ptr/unique_ptr?
-	virtual IExpressionSharedPtr Replace(const Token& name, const IExpressionSharedPtr& exp) const = 0;
+	virtual IExpressionSharedPtr Replace(const Terminal& name, const IExpressionSharedPtr& exp) const = 0;
 
 	virtual ~IExpression() = default;
 };
