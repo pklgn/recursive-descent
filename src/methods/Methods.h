6e8adbf4712e2060f4f48cf62ee38022ef687c60
Pavel Ermakov <petechka_petrov_201@mail.ru>
2022-12-28 20:41:56 +0300

    [dev-methods-ermakov] replace ProgramText with std::istream

--- src/methods/Methods.h
+++ src/methods/Methods.h
@@ -5,18 +5,18 @@ struct Methods
 {
 	Methods() = delete;
 
-	static bool PROG(const ProgramText&, const Position&);
-	static bool VAR(const ProgramText&, const Position&);
-	static bool LISTST(const ProgramText&, const Position&);
-	static bool LISTST_RIGHT(const ProgramText&, const Position&);
-	static bool IDLIST(const ProgramText&, const Position&);
-	static bool IDLIST_RIGHT(const ProgramText&, const Position&);
-	static bool ST(const ProgramText&, const Position&);
-	static bool TYPE(const ProgramText&, const Position&);
-	static bool READ(const ProgramText&, const Position&);
-	static bool WRITE(const ProgramText&, const Position&);
-	static bool ASSIGN(const ProgramText&, const Position&);
-	static bool EXP(const ProgramText&, const Position&);
-	static bool T(const ProgramText&, const Position&);
-	static bool F(const ProgramText&, const Position&);
+	static bool PROG(std::istream&,  Position&);
+	static bool VAR(std::istream&,  Position&);
+	static bool LISTST(std::istream&,  Position&);
+	static bool LISTST_RIGHT(std::istream&,  Position&);
+	static bool IDLIST(std::istream&,  Position&);
+	static bool IDLIST_RIGHT(std::istream&,  Position&);
+	static bool ST(std::istream&,  Position&);
+	static bool TYPE(std::istream&,  Position&);
+	static bool READ(std::istream&,  Position&);
+	static bool WRITE(std::istream&,  Position&);
+	static bool ASSIGN(std::istream&,  Position&);
+	static bool EXP(std::istream&,  Position&);
+	static bool T(std::istream&,  Position&);
+	static bool F(std::istream&,  Position&);
 };
