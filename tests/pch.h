93e21cbb202827c4bc59677ba0c83d44546e757b
beta442 <grandoblivioniv@gmail.com>
2023-01-03 01:23:03 +0300

    https://github.com/pklgn/recursive-descent/issues/4 рефактор тестов

---
+++ tests/pch.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+#include "common_dirs_defines.h"
+
+#include <catch2/catch_all.hpp>
+
+namespace test
+{
+
+namespace StdFs = ::std::filesystem;
+
+using StdPath = StdFs::path;
+
+} // namespace test
